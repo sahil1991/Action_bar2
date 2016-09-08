04ec20cd776b6bfd840d573fa8234faac8b1137c
sahil1991 <anjali.angel3194@gmail.com>
2016-09-08 13:32:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/action_bar2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lenovo.action_bar2;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
